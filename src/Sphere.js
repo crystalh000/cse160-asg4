35e2684e0f19fb6c781dc8eb4948f33de8f900c5
Crystal Huang <chuan147@ucsc.edu>
2024-05-28 21:48:19 -0700

    finished video 4.3

--- src/Sphere.js
+++ src/Sphere.js
@@ -13,11 +13,46 @@ class Sphere{
         gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
         gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
-        var d=Math.PI/10;
-        var dd = Math.PI/10;
+        var d=Math.PI/25;
+        var dd = Math.PI/25;
 
-        for (var t = 0; t < Math.PI; t+=d) {
+        // UV coordinates but decided not to do texture mapping
+
+        /* for (var t = 0; t < Math.PI; t+=d) {
             // points 1,2,3 make a triangle
+            for (var r= 0; r< (2*Math.PI); r+=d ) {
+                var p1 = [Math.sin(t)* Math.cos(r), Math.sin(t) * Math.sin(r), Math.cos(t)];
+                var p2 = [Math.sin(t+dd) * Math.cos(r), Math.sin(t+dd) * Math.sin(r), Math.cos(t+dd)];
+                var p3 = [Math.sin(t) * Math.cos(r+dd), Math.sin(t) * Math.sin(r+dd),Math.cos(t)];
+                var p4 = [Math.sin(t+dd) * Math.cos(r+dd), Math.sin(t+dd) * Math.sin(r+dd), Math.cos(t+dd)];
+                
+                var uv1 = [t/Math.PI, r/(2*Math.PI)];
+                var uv2 = [(t+dd)/Math.PI, r/(2*Math.PI)];
+                var uv3 = [t/Math.PI, (r+dd)/(2*Math.PI)];
+                var uv4 = [(t+dd)/Math.PI, (r+dd)/(2*Math.PI)];
+                
+                var v = [];
+                var uv = [];
+                v = v.concat(p1); uv = uv.concat(uv1);
+                v = v.concat(p2); uv = uv.concat(uv2);
+                v = v.concat(p4); uv = uv.concat(uv4);
+
+                gl.uniform4f(u_FragColor, 1,1, 1, 1);
+                drawTriangle3DUVNormal(v, uv, v); // passing in vertices, uv, and normals (same as vertices) due to sphere shape
+
+                v = []; uv = [];
+                // concat points to make a triangle
+                v = v.concat(p1); uv = uv.concat(uv1);
+                v = v.concat(p2); uv = uv.concat(uv4);
+                v = v.concat(p4); uv = uv.concat(uv3);
+                gl.uniform4f(u_FragColor, 1,0, 0, 1);
+
+                drawTriangle3DUVNormal(v, uv, v); // passing in vertices, uv, and normals (same as vertices) due to sphere shape
+            }
+        } */
+        
+        for (var t = 0; t < Math.PI; t+=d) {
+            //points 1,2,3 make a triangle
             for (var r= 0; r< (2*Math.PI); r+=d ) {
                 var p1 = [Math.sin(t)* Math.cos(r), Math.sin(t) * Math.sin(r), Math.cos(t)];
                 var p2 = [Math.sin(t+dd) * Math.cos(r), Math.sin(t+dd) * Math.sin(r), Math.cos(t+dd)];
@@ -26,7 +61,7 @@ class Sphere{
                 var v = [];
                 var uv = [];
 
-                // concat points to make a triangle
+                //concat points to make a triangle
                 v = v.concat(p1); uv = uv.concat([0,0]);
                 v = v.concat(p2); uv = uv.concat([0,0]);
                 v = v.concat(p4); uv = uv.concat([0,0]);
@@ -40,6 +75,6 @@ class Sphere{
                 gl.uniform4f(u_FragColor, 1,0,0,1);
                 drawTriangle3DUVNormal(v, uv, v);
             }
-        }
+        } 
     }
 }
